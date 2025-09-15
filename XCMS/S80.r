0e1d34b8298db48e82699e3c33c9cefe1c028d96
TommyNHL <research.tommyn.lok@gmail.com>
2025-09-15 18:46:37 +0800

    XCMS test

--- XCMS/S80.r
+++ XCMS/S80.r
@@ -16,12 +16,14 @@ xset <- xcms::xcmsSet(f.in, method = "centWave", ppm = 25,
                       snthr = 10, peakwidth = c(5, 50), mzdiff = 0.01, 
                       nSlaves = 12)
 
+
 ##retention time correction 
 pdf('rector-obiwarp.pdf') 
 
 xsetc <-  xcms::retcor(xset, method = "obiwarp", plottype = "deviation", 
                        profStep = 0.1) 
 dev.off() 
+
 
 peak <- xset@peaks
 write.csv(peak, "Peak-table.csv", row.names = FALSE)
