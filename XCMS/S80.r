d353f6450b368ef734e836254f238172dd1e39dd
TommyNHL <research.tommyn.lok@gmail.com>
2025-09-15 16:20:39 +0800

    XCMS test

---
+++ XCMS/S80.r
@@ -0,0 +1,28 @@
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("xcms")
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("MSnbase")
+
+library(xcms)
+
+f.in <- list.files(pattern = '.(mz[X]{0,1}ML|cdf)', recursive = TRUE)
+
+xset <- xcms::xcmsSet(f.in, method = "centWave", ppm = 25, 
+                      snthr = 10, peakwidth = c(5, 50), mzdiff = 0.01, 
+                      nSlaves = 12)
+
+##retention time correction 
+pdf('rector-obiwarp.pdf') 
+
+xsetc <-  xcms::retcor(xset, method = "obiwarp", plottype = "deviation", 
+                       profStep = 0.1) 
+dev.off() 
+
+peak <- xset@peak 
+write.csv(peak, "Peak-table.csv", row.names = FALSE)
+
